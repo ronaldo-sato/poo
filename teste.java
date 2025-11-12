0e433e065d0f9a032288ee16c789606e5953e322
Ronaldo Mitsuo Sato <ronaldo.sato@gmail.com>
2025-11-12 08:29:54 -0300

    Alterando comentários

--- teste.java
+++ teste.java
@@ -18,8 +18,16 @@
 // feitas, quem pode fazer essas operações, como essas operações são acessadas, 
 // qual a hierarquia de acesso para determinadas operações que fazem parte 
 // daquela classe,...)
+
+// Através da definição de uma classe, descreve-se quais propriedades/atributos 
+// e métodos/ações o objeto terá.
 public class Teste
 {
+    // Todo programa Java tem que ter um método main, que é o método principal 
+    // em que a execução começa, então a partir dele que a execução começa e a 
+    // partir dele que a gente chama outras classes e outras instâncias, que 
+    // podem ou não interagir com outros objetos. 
+
     // Método main: podemos fazer qualquer interação com o uruário, desenvolver
     // funções, chamar essas funções ou da própria linguagem Java, para expor 
     // saídas dos mais variados formatos, fazer acesso a outras classes,...
@@ -27,9 +35,9 @@ public class Teste
     // De modo geral, classes que recebem argumentos de entrada do usuário tem 
     // um método estático.
     {
-        // A entrada é tratada como string, ainda que tenham os conversores, 
-        // Java possui um grande conjunto de classes para tratar entrada do 
-        // usuário e uma delas é a scanner 
+        // A entrada é tratada como String (é uma classe), ainda que tenham os
+        // conversores, Java possui um grande conjunto de classes para tratar
+        // entrada do usuário e uma delas é a scanner
         System.out.println ("Olá Java!");
     }
 }
