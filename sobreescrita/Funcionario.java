9db6f50f9d433f1a97c98c0b3e5b8836793a6e44
Ronaldo Mitsuo Sato <ronaldo.sato@gmail.com>
2025-11-16 16:14:15 -0300

    Adicionando sobreescrita de métodos

---
+++ sobreescrita/Funcionario.java
@@ -0,0 +1,16 @@
+package sobreescrita;
+
+import java.util.Date;
+
+// Subclasse Funcionario (herda a classe Pessoa)
+public class Funcionario extends Pessoa {
+    // Atributos especializados
+    public double salario;
+    public Date dataAdmissao;
+    public String cargo;
+    // Construtor
+    public Funcionario(String nome, String cpf, Date dataNascimento){
+        // Utilizando o comportamento da superclasse (classe mãe)
+        super(nome, cpf, dataNascimento);
+    }
+}
---
+++ sobreescrita/Professor.java
@@ -0,0 +1,15 @@
+package sobreescrita;
+
+import java.util.Date;
+
+// Subclasse Professor (herda a classe Pessoa)
+public class Professor extends Pessoa {
+    // Atributos especializados
+    public double salario;
+    public String disciplina;
+    // Construtor
+    public Professor(String nome, String cpf, Date dataNascimento){
+        // Utilizando o comportamento da superclasse (classe mãe)
+        super(nome, cpf, dataNascimento);
+    }
+}
